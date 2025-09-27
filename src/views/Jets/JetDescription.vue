71770d97fcd9d5ca323812a6d1397192f6620297
FERN1701 <argonfernando453@gmail.com>
2025-09-27 17:22:18 +0800

    latest

--- src/views/Jets/JetDescription.vue
+++ src/views/Jets/JetDescription.vue
@@ -2,15 +2,14 @@
 <section class="container py-5">
     <div class="container-fluid">
         <h1>Description</h1>
-        <h6>Name : {{jet.name_unit}}</h6>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, delectus fugit numquam possimus odio suscipit odit vitae ipsam veniam asperiores perspiciatis, porro illo, laborum dicta consequatur minus debitis officiis. Impedit?</p>
         <div class="row py-5">
             <div class="col-sm-6 mb-5">
                 <img :src="jet.image" alt="" class="img-fluid">
             </div>
             <div class="col-sm-6">
-                <h5>Cabin Space</h5>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, error sequi quam voluptate aliquid quod! Dignissimos consequuntur omnis ipsam rem ipsa quod eius molestias quidem aperiam non, necessitatibus cum exercitationem.</p>
+                <h5>{{jet.name_unit}}</h5>
+                <p>{{jet.description}}</p>
             </div>
         </div>
     </div>
@@ -21,6 +20,7 @@
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { airlineStore } from '../../stores/control';
+import Jets from '@/components/Jets.vue'
 
 const route = useRoute()           // ✅ you need this
 const airline = airlineStore()
